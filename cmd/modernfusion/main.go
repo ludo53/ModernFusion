6143aadf31b15163d3a9742b871eb08d42df680d
Ludo <97764715+ludo53@users.noreply.github.com>
2025-08-02 20:00:16 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/modernfusion/main.go
@@ -0,0 +1,23 @@
+// cmd/modernfusion/main.go
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+
+	"modernfusion/internal/modernfusion"
+)
+
+func main() {
+	verbose := flag.Bool("verbose", false, "Enable verbose logging")
+	input := flag.String("input", "", "Input file path")
+	output := flag.String("output", "", "Output file path")
+	flag.Parse()
+
+	app := modernfusion.NewApp(*verbose)
+	if err := app.Run(*input, *output); err != nil {
+		log.Printf("Error: %v", err)
+		os.Exit(1)
+	}
+}
